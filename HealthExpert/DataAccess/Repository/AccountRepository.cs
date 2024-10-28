fc2e4a3f75f68152b7c7acaa6ecce7db6575ad8f
DE170328 <92565245+DE170328@users.noreply.github.com>
2024-10-28 23:43:04 +0700

    addAPI

---
+++ HealthExpert/DataAccess/Repository/AccountRepository.cs
@@ -0,0 +1,34 @@
+﻿using BussinessObject.Model.Authen;
+using BussinessObject.Model.ModelUser;
+using DataAccess.DAO;
+using DataAccess.Repository.IRepository;
+
+namespace DataAccess.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public Account Authenticate(UserLogin login) => AccountDAO.Authenticate(login);
+
+        public bool CheckAccount(string userName, string password) => AccountDAO.CheckAccount(userName, password);
+
+        public List<Account> GetListAccount() => AccountDAO.GetListAccount();
+
+        public Account GetAccountById(Guid id) => AccountDAO.GetAccountById(id);
+
+        public void UpdateAccount(Account account) => AccountDAO.UpdateAccount(account);
+
+        public void DeleteAccount(Guid id) => AccountDAO.DeleteAccount(id);
+
+        public void AddAccount(Account account) => AccountDAO.AddAccount(account);
+
+        public Guid GetAccountId(string userName)
+        {
+            return AccountDAO.GetAccountId(userName);
+        }
+
+        public List<Account> GetAccountByEmail(string email)
+        {
+            return AccountDAO.GetAccountByEmail(email);
+        }
+    }
+}
