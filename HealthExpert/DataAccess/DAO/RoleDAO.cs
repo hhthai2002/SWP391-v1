7a339e98b1b3ca0536759df3813133fcacb95991
DE170328 <92565245+DE170328@users.noreply.github.com>
2024-10-28 23:37:51 +0700

    updb

---
+++ HealthExpert/DataAccess/DAO/RoleDAO.cs
@@ -0,0 +1,46 @@
+﻿using BussinessObject.ContextData;
+using BussinessObject.Model.Authen;
+
+namespace DataAccess.DAO
+{
+    public class RoleDAO
+    {
+        //Get Role List Data
+        public static List<Role> GetRoleList()
+        {
+            var listRole = new List<Role>();
+            try
+            {
+                using (var ctx = new HealthExpertContext())
+                {
+                    listRole = ctx.Roles.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return listRole;
+        }
+
+        //Get Role Data by Id
+        public static Role GetRoleById(int id)
+        {
+            var role = new Role();
+            try
+            {
+                using (var ctx = new HealthExpertContext())
+                {
+                    role = ctx.Roles.FirstOrDefault(role => role.roleId == id);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return role;
+        }
+    }
+}
