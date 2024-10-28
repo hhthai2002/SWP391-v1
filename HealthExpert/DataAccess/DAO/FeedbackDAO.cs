84f4b421529b7f719da4468423de748cabfbc267
helluHuyen <luuhuyenvt2004@gmail.com>
2024-10-28 23:54:35 +0700

    feedback order post

---
+++ HealthExpert/DataAccess/DAO/FeedbackDAO.cs
@@ -0,0 +1,94 @@
+﻿using BussinessObject.ContextData;
+using BussinessObject.Model.ModelCourse;
+
+namespace DataAccess.DAO
+{
+    public class FeedbackDAO
+    {
+        //Add Feedback by accountId and courseId
+        //public static void AddFeedback(Guid accountId, string courseId, Feedback feedback)
+        //{
+        //    try
+        //    {
+        //        using (var context = new HealthExpertContext())
+        //        {
+        //            var account = context.Accounts.FirstOrDefault(account => account.accountId == accountId);
+        //            var course = context.Courses.FirstOrDefault(course => course.courseId == courseId);
+        //            feedback.account = account;
+        //            feedback.course = course;
+        //            context.Feedbacks.Add(feedback);
+        //            context.SaveChanges();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception(ex.Message);
+        //    }
+        //}
+        public static void AddFeedback(Feedback feedback)
+        {
+            using (var context = new HealthExpertContext())
+            {
+                context.Feedbacks.Add(feedback);
+                context.SaveChanges();
+            }
+        }
+
+        public static void DeleteFeedback(Feedback feedback)
+        {
+            using (var context = new HealthExpertContext())
+            {
+                context.Feedbacks.Remove(feedback);
+                context.SaveChanges();
+            }
+        }
+
+        public static List<Feedback> GetFeedbacks()
+        {
+            using (var context = new HealthExpertContext())
+            {
+                return context.Feedbacks.ToList();
+            }
+        }
+
+        public static Feedback GetFeedbackById(Guid feedbackId)
+        {
+            using (var context = new HealthExpertContext())
+            {
+                return context.Feedbacks.FirstOrDefault(f => f.feedbackId == feedbackId);
+            }
+        }
+
+
+        public static void UpdateFeedback(Feedback feedback)
+        {
+            using (var context = new HealthExpertContext())
+            {
+                context.Feedbacks.Update(feedback);
+                context.SaveChanges();
+            }
+        }
+
+        //update feedback by accountId and feedbackId
+        //public static void UpdateFeedback(Guid accountId, Guid feedbackId, Feedback feedback)
+        //{
+        //    try
+        //    {
+        //        using (var context = new HealthExpertContext())
+        //        {
+        //            var account = context.Accounts.FirstOrDefault(account => account.accountId == accountId);
+        //            var feedbackUpdate = context.Feedbacks.FirstOrDefault(feedback => feedback.feedbackId == feedbackId);
+        //            feedbackUpdate.account = account;
+        //            feedbackUpdate.feedbackContent = feedback.feedbackContent;
+        //            feedbackUpdate.feedbackDate = feedback.feedbackDate;
+        //            feedbackUpdate.feedbackRating = feedback.feedbackRating;
+        //            context.SaveChanges();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception(ex.Message);
+        //    }
+        //}
+    }
+}
---
+++ HealthExpert/DataAccess/DAO/OrderDAO.cs
@@ -0,0 +1,102 @@
+﻿using BussinessObject.ContextData;
+using BussinessObject.Model.ModelPayment;
+
+namespace DataAccess.DAO
+{
+    public class OrderDAO
+    {
+        //Get Order List
+        public static List<Order> GetAllOrder()
+        {
+            var listOrder = new List<Order>();
+            try
+            {
+                using (var ctx = new HealthExpertContext())
+                {
+                    listOrder = ctx.Orders.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return listOrder;
+        }
+
+        //Get Order by Id
+        public static Order GetOrderById(Guid id)
+        {
+            var order = new Order();
+            try
+            {
+                using (var ctx = new HealthExpertContext())
+                {
+                    order = ctx.Orders.FirstOrDefault(x => x.orderId == id);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return order;
+        }
+
+        //Add Order
+        public static void AddOrder(Order order)
+        {
+            try
+            {
+                using (var ctx = new HealthExpertContext())
+                {
+                    ctx.Orders.Add(order);
+                    ctx.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //Update Order
+        public static void UpdateOrder(Guid id, Order order)
+        {
+            try
+            {
+                using (var ctx = new HealthExpertContext())
+                {
+                    if (GetOrderById(id) != null)
+                    {
+                        ctx.Orders.Add(order);
+                        ctx.Entry(order).State =
+                            Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //Delete Order
+        public static void DeleteOrder(Guid id)
+        {
+            try
+            {
+                using (var ctx = new HealthExpertContext())
+                {
+                    if (GetOrderById(id) != null)
+                    {
+                        ctx.Orders.Remove(GetOrderById(id));
+                        ctx.SaveChanges();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
