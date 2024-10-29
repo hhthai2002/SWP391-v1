3b4e301ed9af06737a88483cf364cc5c2b119e1b
Thanh Hiếu <122498689+ThanhHieu1522@users.noreply.github.com>
2024-10-29 08:58:53 +0700

    up Repository

---
+++ HealthExpert/DataAccess/Repository/BillRepository.cs
@@ -0,0 +1,19 @@
+﻿using BussinessObject.Model.ModelPayment;
+using DataAccess.DAO;
+using DataAccess.Repository.IRepository;
+
+namespace DataAccess.Repository
+{
+    public class BillRepository : IBillRepository
+    {
+        public void DeleteBill(Guid id) => BillDAO.DeleteBill(id);
+
+        public List<Bill> GetAllBills() => BillDAO.GetBills();
+
+        public Bill GetBillById(Guid id) => BillDAO.GetBillById(id);
+
+        public void InsertBill(Bill bill) => BillDAO.InsertBill(bill);
+
+        public void UpdateBill(Guid id, Bill bill) => BillDAO.UpdateBill(id, bill);
+    }
+}
---
+++ HealthExpert/DataAccess/Repository/RoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using BussinessObject.Model.Authen;
+using DataAccess.DAO;
+using DataAccess.Repository.IRepository;
+
+namespace DataAccess.Repository
+{
+    public class RoleRepository : IRoleRepository
+    {
+        public List<Role> GetAllRoles() => RoleDAO.GetRoleList();
+
+        public Role GetRoleById(int id) => RoleDAO.GetRoleById(id);
+    }
+}
