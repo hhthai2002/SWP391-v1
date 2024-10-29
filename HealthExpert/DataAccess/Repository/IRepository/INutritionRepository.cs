c17c1224e82fb42b68ae65fde5cd7ee6cca2dee7
fuuko1311 <nskp2k3@gmail.com>
2024-10-29 10:02:32 +0700

    DRepo SCenter

---
+++ HealthExpert/DataAccess/Repository/IRepository/INutritionRepository.cs
@@ -0,0 +1,15 @@
+﻿using BussinessObject.Model.ModelNutrition;
+
+namespace DataAccess.Repository.IRepository
+{
+    public interface INutritionRepository
+    {
+        void AddNutrition(Nutrition nutrition);
+        void UpdateNutrition(string id, Nutrition nutrition);
+        void DeleteNutrition(string id);
+        List<Nutrition> GetAllNutritions();
+        Nutrition GetNutritionById(string id);
+        Nutrition GetNutritionByTitle(string title);
+        void CreateNutritionBySessonId(Nutrition nutrition);
+    }
+}
