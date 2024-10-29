35bdf9347a2ba26ce1d40afb12bb5070b00c9ec4
helluHuyen <luuhuyenvt2004@gmail.com>
2024-10-29 09:29:38 +0700

    repository learner

---
+++ HealthExpert/DataAccess/Repository/IRepository/IBMIRepository.cs
@@ -0,0 +1,13 @@
+﻿using BussinessObject.Model.ModelUser;
+
+namespace DataAccess.Repository.IRepository
+{
+    public interface IBMIRepository
+    {
+        List<BMI> GetBMI();
+        BMI GetBMIById(int bmiId);
+        void AddBMI(BMI bmi);
+        void UpdateBMI(BMI bmi);
+        void DeleteBMI(int bmiId);
+    }
+}
---
+++ HealthExpert/DataAccess/Repository/IRepository/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using BussinessObject.Model.ModelPayment;
+
+namespace DataAccess.Repository.IRepository
+{
+    public interface IOrderRepository
+    {
+        List<Order> GetAllOrders();
+        Order GetOrderById(Guid id);
+        void AddOrder(Order order);
+        void UpdateOrder(Guid id, Order order);
+        void DeleteOrder(Guid id);
+    }
+}
