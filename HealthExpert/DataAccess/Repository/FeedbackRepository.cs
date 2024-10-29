43e31253c3e84b441019550d4ccc575f17e1772a
helluHuyen <luuhuyenvt2004@gmail.com>
2024-10-29 09:08:34 +0700

    repository

---
+++ HealthExpert/DataAccess/Repository/FeedbackRepository.cs
@@ -0,0 +1,39 @@
+﻿using BussinessObject.Model.ModelCourse;
+using DataAccess.DAO;
+using DataAccess.Repository.IRepository;
+
+namespace DataAccess.Repository
+{
+    public class FeedbackRepository : IFeedbackRepository
+    {
+        public void AddFeedback(Feedback feedback)
+        {
+            FeedbackDAO.AddFeedback(feedback);
+        }
+
+        public void DeleteFeedback(Feedback feedback)
+        {
+            FeedbackDAO.DeleteFeedback(feedback);
+        }
+
+        public List<Feedback> GetFeedbacks()
+        {
+            return FeedbackDAO.GetFeedbacks();
+        }
+
+        public Feedback GetFeedbackById(Guid feedbackId)
+        {
+            return FeedbackDAO.GetFeedbackById(feedbackId);
+        }
+
+        public void UpdateFeedback(Feedback feedback)
+        {
+            FeedbackDAO.UpdateFeedback(feedback);
+        }
+
+        //public void AddFeedback(Guid accountId, string courseId, Feedback feedback)
+        //{
+        //    FeedbackDAO.AddFeedback(accountId, courseId, feedback);
+        //}
+    }
+}
