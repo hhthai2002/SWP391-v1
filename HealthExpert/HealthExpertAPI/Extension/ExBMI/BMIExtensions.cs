6572b93b1d426dbc2a8c6df6c6ba519c8022ded6
helluHuyen <luuhuyenvt2004@gmail.com>
2024-10-29 09:37:50 +0700

    exbmi feddback

---
+++ HealthExpert/HealthExpertAPI/Extension/ExBMI/BMIExtensions.cs
@@ -0,0 +1,49 @@
+﻿using BussinessObject.Model.ModelUser;
+using HealthExpertAPI.DTO.DTOBMI;
+
+namespace HealthExpertAPI.Extension.ExBMI
+{
+    public static class BMIExtensions
+    {
+        public static BMIDTO ToBMIDTO(this BMI bmi)
+        {
+            return new BMIDTO
+            {
+                bmiId = bmi.bmiId,
+                weight = bmi.weight,
+                height = bmi.height,
+                bmiValue = bmi.bmiValue,
+                bmiStatus = bmi.bmiStatus,
+                createDate = bmi.bmiDate,
+                accountId = bmi.accountId
+            };
+        }
+
+        public static BMI ToRegisterBMI(this BMIDTO bmiDTO)
+        {
+            return new BMI
+            {
+                bmiId = bmiDTO.bmiId,
+                weight = bmiDTO.weight,
+                height = bmiDTO.height,
+                bmiValue = bmiDTO.bmiValue,
+                bmiStatus = bmiDTO.bmiStatus,
+                bmiDate = bmiDTO.createDate,
+                accountId = bmiDTO.accountId
+            };
+        }
+
+        public static BMI ToUpdateBMI(this BMIDTOUpdate bmiDTO)
+        {
+            return new BMI
+            {
+                weight = bmiDTO.weight,
+                height = bmiDTO.height,
+                bmiValue = bmiDTO.bmiValue,
+                bmiStatus = bmiDTO.bmiStatus,
+                bmiDate = bmiDTO.createDate,
+            };
+        }
+
+    }
+}
