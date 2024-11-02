2a7d6476d5873e213821cc5a762e75ca0bd8aa52
Thanh Hiếu <122498689+ThanhHieu1522@users.noreply.github.com>
2024-11-02 16:48:07 +0700

    Update ĐKcentral

--- HealthExpert/HealthExpertAPI/DTO/DTOAccount/ServiceCenterRegistrationDTO.cs
+++ HealthExpert/HealthExpertAPI/DTO/DTOAccount/ServiceCenterRegistrationDTO.cs
@@ -7,22 +7,35 @@ namespace HealthExpertAPI.DTO.DTOAccount
     {
         [Required]
         public string userName { get; set; } = string.Empty;
+
         [Required]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")]
         [DataType(DataType.Password)]
         public string password { get; set; } = string.Empty;
+
         [Required, Compare("password")]
         public string confirmPassword { get; set; } = string.Empty;
+
         [Required, EmailAddress]
         public string email { get; set; } = string.Empty;
+
         public string fullName { get; set; } = string.Empty;
+
         public string phone { get; set; } = string.Empty;
+
+        public string address { get; set; } = string.Empty; // Thêm thuộc tính address
+
         public bool gender { get; set; }
+
         [JsonIgnore]
         public DateTime birthDate { get; set; } = DateTime.Now;
+
         public string bankNumber { get; set; } = string.Empty;
+
         public string bankName { get; set; } = string.Empty;
+
         public int roleId { get; set; }
+
         public bool isActive { get; set; }
     }
 }
